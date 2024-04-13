9dacba36431913103b76a39125e21362a57c4629
Yana <yanakirova1@gmail.com>
2024-04-13 11:51:57 +0100

    first commit

---
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    //ad mapping for listing employees
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        //get employees from db
+        List<Employee> theEmployees = employeeService.findAll();
+        //add to spring model
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+}
