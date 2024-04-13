708e28e07b0c36a7ce4be4a4ed8c65c13b155f7c
Yana <yanakirova1@gmail.com>
2024-04-13 13:21:48 +0100

    saving employee in db

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,6 +28,24 @@ public class EmployeeController {
         //add to spring model
         theModel.addAttribute("employees", theEmployees);
         //return Thymeleaf template
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+        public String showFormForAdd(Model theModel){
+            //create model attribute to bind form data
+            Employee employee = new Employee();
+
+            theModel.addAttribute("employee", employee);
+
+            return "employees/employee-form";
+        }
+
+    @PostMapping("/employees/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        //save employee
+        employeeService.save(employee);
+        //use redirection to prevent duplicate submissions with Post/redirect/get pattern
+        return "redirect:/employees/list";
     }
 }
